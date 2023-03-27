c7a07edb967f591a44fdb2a120a361b696abce1e
Ravi Gunasena <ravigunz@gmail.com>
2023-03-27 12:19:20 +0530

    Python random and math libraries

---
+++ random_number.py
@@ -0,0 +1,12 @@
+import random
+import math
+
+# Generate a random number
+
+random_number = random.random()
+
+# Calculate teh square root of the generated random nuber
+sqrt_value = math.sqrt(random_number)
+
+print(random_number)
+print(sqrt_value)
